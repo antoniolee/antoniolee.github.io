2a5e110ae1e780cf5c7f800c973c4500f67c770a
Antonio <antoniolee@outlook.com>
2018-04-09 11:30:05 -0700

    debugging barba.js
    • I believe the problem was with the URLs being set with site.url. Should be site.baseurl

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,22 +1,4 @@
-// $( document ).ready(function() {
-//     function openNav() {
-//         $('.overlay').width("100%");
-//     }
-
-//     function closeNav() {
-//         $('.overlay').width("0%");
-//     }
-
-    
-//     $('.btn-open').click(openNav);
-//     $('.btn-close').click(closeNav);
-//     $('.overlay > div.overlay-content > a').click(closeNav);
-
-// });
-
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM fully loaded and parsed");
-
+$( document ).ready(function() {
     function openNav() {
         $('.overlay').width("100%");
     }
@@ -30,3 +12,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
     $('.btn-close').click(closeNav);
     $('.overlay > div.overlay-content > a').click(closeNav);
 });
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+
+//     function openNav() {
+//         $('.overlay').width("100%");
+//     }
+
+//     function closeNav() {
+//         $('.overlay').width("0%");
+//     }
+
+    
+//     $('.btn-open').click(openNav);
+//     $('.btn-close').click(closeNav);
+//     $('.overlay > div.overlay-content > a').click(closeNav);
+// });
